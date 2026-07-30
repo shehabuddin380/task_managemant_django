c0a8495532cf65319b429aeb6683cba9bfa188cd
Shihab Ahmed <shehabuddin380@gmail.com>
2026-07-30 23:17:35 +0600

    update sinals

--- tasks/signals.py
+++ tasks/signals.py
@@ -2,6 +2,9 @@ from django.db.models.signals import post_save, pre_save, m2m_changed, post_dele
 from django.dispatch import receiver
 from django.core.mail import send_mail
 from tasks.models import Task
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 @receiver(m2m_changed, sender=Task.assigned_to.through)
@@ -12,14 +15,10 @@ def notify_employees_on_task_creation(sender, instance, action, **kwargs):
         assigned_emails = [emp.email for emp in instance.assigned_to.all()]
         # print("Checking....", assigned_emails)
 
-        send_mail(
-            "New Task Assigned",
-            f"You have been assigned to the task: {instance.title}",
-            "slashupdates@gmail.com",
-            assigned_emails,
-            fail_silently=False,
-        )
+        if not assigned_emails:
+            return
 
+ 
 
 @receiver(post_delete, sender=Task)
 def delete_associate_details(sender, instance, **kwargs):
