b670bee9b49d8b0ef2d18668fed52f907cd6cab8
Shihab Ahmed <shehabuddin380@gmail.com>
2026-08-01 23:41:17 +0600

    custom filters add has group

--- users/templatetags/custom_filters.py
+++ users/templatetags/custom_filters.py
@@ -24,3 +24,9 @@ def has_group(user, group_name):
     if not user.is_authenticated:
         return False
     return user.groups.filter(name=group_name).exists()
+
+@register.filter(name='has_group')
+def has_group(user, group_name):
+    if not user.is_authenticated:
+        return False
+    return user.groups.filter(name=group_name).exists()
